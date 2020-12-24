548ae44e0ad6c6ea63596956c9ef141eb1a00b3a
Davide Sarra <davide.sarra@gmail.com>
2020-12-24 18:05:55 +0000

    Create route to delete topic

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -17,7 +17,7 @@ defmodule DiscussWeb.TopicController do
     case Discuss.Repo.insert(changeset) do
       {:ok, _topic} ->
         conn
-        |> put_flash(:info, "Topic Created")
+        |> put_flash(:info, "Topic created")
         |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
@@ -41,5 +41,13 @@ defmodule DiscussWeb.TopicController do
       {:error, changeset} ->
         render(conn, "edit.html", changeset: changeset, topic: old_topic)
     end
+  end
+
+  def delete(conn, %{"id" => topic_id}) do
+    Discuss.Repo.get!(Topic, topic_id) |> Discuss.Repo.delete!
+
+    conn
+    |> put_flash(:info, "Topic deleted")
+    |> redirect(to: topic_path(conn, :index))
   end
 end
