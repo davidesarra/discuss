29603b470a19ca68a9212b5f568403af1bea0c35
Davide Sarra <davide.sarra@gmail.com>
2020-12-28 22:20:50 +0000

    Update comments as soon as a comment is made

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -60,6 +60,8 @@ const createSocket = (topicId) => {
     .receive("ok", resp => { renderComments(resp.comments) })
     .receive("error", resp => { console.log("Unable to join", resp) })
 
+  channel.on(`comments:${topicId}:new`, renderComment)
+
   document.querySelector("button").addEventListener("click", () => {
     const content  = document.querySelector("textarea").value;
     channel.push("comment:add", { content: content })
@@ -68,13 +70,22 @@ const createSocket = (topicId) => {
 
 function renderComments(comments) {
   const renderedComments = comments.map(comment => {
-    return `
-      <li class="collection-item">
-        ${comment.content}
-      </li>
-    `;
+    return commentTemplate(comment);
   })
   document.querySelector(".collection").innerHTML = renderedComments.join("");
+}
+
+function renderComment(event) {
+  const renderedComment = commentTemplate(event.comment);
+  document.querySelector(".collection").innerHTML += renderedComment;
+}
+
+function commentTemplate(comment) {
+  return `
+    <li class="collection-item">
+      ${comment.content}
+    </li>
+  `;
 }
 
 window.createSocket = createSocket;
