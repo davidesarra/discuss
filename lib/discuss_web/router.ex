37786997431375c1fdee43438b7cb83bf7e4763d
Davide Sarra <davide.sarra@gmail.com>
2020-12-29 11:02:33 +0000

    Authenticate sign-in users on sockets

--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -8,6 +8,7 @@ defmodule DiscussWeb.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug DiscussWeb.Plugs.SetUser
+    plug :put_user_token
   end
 
   pipeline :api do
@@ -26,6 +27,15 @@ defmodule DiscussWeb.Router do
     get "/:provider", AuthController, :request
     get "/:provider/callback", AuthController, :callback
     delete "/sign_out", AuthController, :sign_out
+  end
+
+  defp put_user_token(conn, _params) do
+    if user = conn.assigns[:user] do
+      token = Phoenix.Token.sign(conn, "user socket", user.id)
+      assign(conn, :user_token, token)
+    else
+      conn
+    end
   end
 
   # Other scopes may use custom stacks.
